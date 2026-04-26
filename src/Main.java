5a903bcd147ac04d34e1d3e673016eb56b6a9ef7
Ashar Zafri <asharzafri45@gmail.com>
2026-04-26 12:39:09 +0100

    variables practice

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Grade g1 = new Grade("MATH01", 75.0);
-        System.out.println(g1.courseCode);
-        System.out.println(g1.score);
-        System.out.println(g1.isApproved);
-        System.out.println(g1.getLetterGrade());
+        String studentName = "Ashar";
+        System.out.println("Name: " + studentName);
+
+        studentName = "Zafri";  // birthday — no 'int' keyword this time
+        System.out.println("Name: " + studentName);
     }
-}0
+}
