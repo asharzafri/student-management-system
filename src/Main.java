614dd85483ecaa189d1b16b3981831dd3d08376d
Ashar Zafri <asharzafri45@gmail.com>
2026-04-26 19:29:52 +0100

    Lesson 9: ArrayList for students and professors

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,43 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class Main {
 
     public static void main(String[] args) {
-      Student s1 = new Student("Ashar", "Zafri",1001,3.75);
-      Student s2 = new Student("Sara", "Khan",1002,3.2);
-      Professor p1 = new Professor("John", "Doe",8001,"English");
-      Professor p2 = new Professor("Mike", "Jack",8002,"Maths");
+      ArrayList<Student> students = new ArrayList<>();
+      ArrayList<Professor> professors = new ArrayList<>();
 
-        System.out.println(s1.getFullName()+" - "+s1.getStanding());
-        System.out.println(s2.getFullName()+" - "+s2.getStanding());
-        System.out.println("Professor: " + p1.getFullName());
-        System.out.println("Professor: " + p2.getFullName());
+
+      //Adding student data to student Array
+      students.add(new Student("Ashar", "Zafri", 1001, 3.75));
+      students.add(new Student("Sara", "Khan", 1002, 3.2));
+      students.add(new Student("John", "Doe", 1003, 2.1));
+
+      // Adding data to Professor Array
+      professors.add(new Professor("Mike", "Murry",8001,"Math"));
+      professors.add(new Professor("John", "Doe",8002, "English"));
+      professors.add(new Professor("Peter", "Pan",8003, "History"));
+
+      System.out.println("Student List");
+      //Total number of students
+      System.out.println("Total Number of Students: " + students.size());
+      //Printing everything data from student Array
+      for(Student s : students){
+            System.out.println(s.getFullName() + " - GPA: " + s.gpa);
+      }
+
+      //Break line
+      System.out.println("-------");
+
+
+      System.out.println("Professor List");
+      //Total number of professors
+      System.out.println("Total Number of Professors: " + professors.size());
+
+      //Printing all data from professor Array
+        for(Professor p : professors){
+            System.out.println("Full Name: "+ p.getFullName()+ " - Department: "+ p.department);
+        }
 
 
     }
