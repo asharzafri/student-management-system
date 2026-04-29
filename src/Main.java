944eb510adab91d5867850f118794a834ed34932
Ashar Zafri <asharzafri45@gmail.com>
2026-04-29 22:53:02 +0100

    Lesson 11: SMS menu system with add and view students

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -5,35 +6,51 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-
-        // Welcome message
-        System.out.println("Welcome to Ashar's  Student Management System");
-
-        // Asking user to enter the data.
-        System.out.print("Enter student first name: ");
-        String firstName = scanner.nextLine();
-
-        System.out.print("Enter student last name: ");
-        String lastName = scanner.nextLine();
-
-        System.out.print("Enter student ID: ");
-        int id = scanner.nextInt();
-
-        System.out.print("Enter GPA: ");
-        double gpa = scanner.nextDouble();
-
-        // Creating a Student object from the input
-        Student student = new Student(firstName, lastName, id, gpa);
+        ArrayList<Student> students=new ArrayList<Student>();
 
 
+      while(true) {
+          // Welcome message
+          System.out.println("=== Student Management System ===");
+          System.out.println("Please select one of the following:");
+          System.out.println("1. Add Student");
+          System.out.println("2. View Student");
+          System.out.println("3. Exit");
+          int choice=scanner.nextInt();
+           if(choice==1) {
+               //asking user to enter Data
+               System.out.println("Enter Student ID");
+               int id=scanner.nextInt();
+               scanner.nextLine(); // add this line to clear the buffer
+               System.out.println("Enter Student first name");
+               String firstname=scanner.nextLine();
+               System.out.println("Enter Student Surname");
+               String lastname=scanner.nextLine();
+               System.out.print("Enter GPA: ");
+               double gpa = scanner.nextDouble();
 
-        //Printing entered Data
-        System.out.println("--- Student Created ---");
-        System.out.println("Name: "+ student.getFullName());
-        System.out.println("ID: "+student.studentId);
-        System.out.println("GPA: "+student.gpa);
-        System.out.println("Standing: "+student.getStanding());
+               //creating a student object from data shared by user
+               Student student = new Student(firstname,lastname,id,gpa);
+               //Adding data to Array list
+               students.add(student);
+           }
+           else if(choice==2) {
+               //Using size() method to find the total of array Student
+               System.out.println("Total number of students in system: "+students.size());
 
+               for(Student s:students) {
+                   System.out.println(s.getFullName() + " - GPA: " + s.gpa + " - " + s.getStanding());
+               }
+           }
+           // Eixt
+          else if(choice==3) {
+              System.out.println("Thank you Good Bye!");
+              break;
+
+          }
+
+
+      }
 
     }
 }
